7531659223cd4e6c2af51ff632dc79e6771cb1cf
Marco Acea <aceamarco@gmail.com>
2023-09-21 09:26:08 -0700

    new changes

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 const express = require('express')
 const morgan = require('morgan');
+const cors = require('cors')
+
 const app = express()
 app.use(express.json())
+app.use(cors())
 
 
 // Define a custom log format that includes request body for POST requests
